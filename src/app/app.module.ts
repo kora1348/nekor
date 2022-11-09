91a7e48470af4c19556a1060b71068abea6f8c2c
kora1348 <amelianekor@gmail.com>
2022-11-09 14:38:01 +0100

    en test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgxsStoragePluginModule } from "@ngxs/storage-plugin";
+import { NgxsModule } from "@ngxs/store";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CanActivateTeam } from "./core/canActivateTeam.guard";
+import { AppHttpInterceptor } from "./core/interceptor/app-http.interceptor";
+import { IsActivate } from "./core/isActivate.guard";
+import { DefaultModule } from "./layouts/default/default.module";
+import { FullwidthModule } from "./layouts/fullwidth/fullwidth.module";
+import { SessionState } from "./store/session/session.state";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DefaultModule } from './layouts/default/default.module';
-import { FullwidthModule } from './layouts/fullwidth/fullwidth.module';
 
 @NgModule({
   declarations: [
@@ -14,9 +22,29 @@ import { FullwidthModule } from './layouts/fullwidth/fullwidth.module';
     BrowserModule,
     AppRoutingModule,
     DefaultModule,
-    FullwidthModule
+    FullwidthModule,
+    HttpClientModule,
+    FormsModule,
+    NgxsModule.forRoot([SessionState], {
+      developmentMode: true
+    }),
+    NgxsStoragePluginModule.forRoot({
+      key: 'session'
+    }),
+
   ],
-  providers: [],
+  providers: [
+    CanActivateTeam,
+    IsActivate,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AppHttpInterceptor,
+      multi: true
+
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
--- src/app/views/views/signin/signin.component.ts
+++ src/app/views/views/signin/signin.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { SessionService } from 'src/app/services/sessions.service';
+import { Login } from './login';
+
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +13,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  loginForm: Login = new Login();
+  ngUnsubscribe = new Subject<void>();
 
-  constructor() { }
+  constructor(private log : SessionService, private store : Store, private router: Router) {
+
+  }
 
   ngOnInit() {
   }
 
+  login():void {
+
+  }
 }
+
+
+ 
